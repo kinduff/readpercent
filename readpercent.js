79b4a6d7ebb48b94d7014075d81400d13d05d18f
kinduff <abarcadabra@gmail.com>
2014-04-16 17:40:59 -0500

    Mod suggestions by @alv-c

--- readpercent.js
+++ readpercent.js
@@ -1,29 +1,26 @@
+/**
+ * readpercent v0.1 http://kinduff.github.io/readpercent
+ * author: @kinduff
+ */
 (function ($) {
     function get_percent(container) {
         var posTop = $(window).scrollTop() - ($(container).offset().top - $(window).height());
         var percent = (posTop * 100) / $(container).height();
-        return percent;
-    }
-
-    function limit_percent(percent) {
-        percent = (percent <= 0) ? 0 : percent;
-        percent = (percent >= 100) ? 100 : percent;
+        percent = Math.min(100, Math.max(0, percent));
         return percent;
     }
     $.fn.readpercent = function (receiver, bar) {
         var content = this.selector, percent;
         if (! receiver) { // Get raw percent (0-100)
-            percent = limit_percent(get_percent(content));
-            return percent;
+            return get_percent(content);
         } else if (receiver === true) { // Get rounded percent (0-100)
-            percent = limit_percent(Math.round(get_percent(content)));
-            return percent;
+            return ~~get_percent(content);
         } else {
             $(window).bind("scroll.globalMessage", {
                 receiver: receiver,
                 container: content
             }, function (e) {
-                percent = limit_percent(get_percent(e.data.container));
+                percent = get_percent(e.data.container);
                 if (bar) { // Change width percent of an element
                     $(receiver + ' ' + bar).css('width', percent + '%');
                 } else { // Fill HTML5 Progress Element
